fa459a92cc3ff309569889af5f58ae4f5d41de82
gabbybamps <gabbybamps@gmail.com>
2021-09-26 14:42:19 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHanlder = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHanlder} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
